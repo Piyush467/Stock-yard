fc46cd24b74721837e1e880752370d8b42f42b74
Piyush467 <piyushmehrotra61@gmail.com>
2026-02-04 22:24:20 +0530

    chore: setup monorepo for frontend backend dashboard

---
+++ frontend/src/landing_page/products/Universe.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+
+function Universe() {
+    return ( 
+          <div className="container mb-5 mt-5">
+      <div className="row text-center mt-5">
+        <h1 className="fs-4">The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/smallcaseLogo.png"
+            style={{ width: "50%", paddingBottom: "6px" }}
+          ></img>
+          <p className="text-small text-muted mt-3">
+            Thematic investing platform
+            <br />
+            that helps you invest in diversified
+            <br />
+            baskets of stocks on ETFs.
+          </p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/streakLogo.png" style={{ width: "40%" }}></img>
+          <p className="text-small text-muted mt-3">
+            Systematic trading platform
+            <br />
+            that allows you to create and backtest
+            <br />
+            strategies without coding.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/sensibullLogo.svg"
+            style={{ width: "50%", paddingBottom: "15px" }}
+          ></img>
+          <p className="text-small text-muted mt-3">
+            Options trading platform that lets you
+            <br />
+            create strategies, analyze positions, and examine
+            <br />
+            data points like open interest, FII/DII, and more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/zerodhaFundhouse.png"
+            style={{ width: "50%", paddingBottom: "15px" }}
+          ></img>
+          <p className="text-small text-muted mt-3">
+            Our asset management venture
+            <br />
+            that is creating simple and transparent index
+            <br />
+            funds to help you save for your goals.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/goldenpiLogo.png"
+            style={{ width: "50%", paddingBottom: "15px" }}
+          ></img>
+          <p className="text-small text-muted mt-3">
+            Investment research platform
+            <br />
+            that offers detailed insights on stocks,
+            <br />
+            sectors, supply chains, and more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img
+            src="media/dittoLogo.png"
+            style={{ width: "50%", paddingBottom: "15px" }}
+          ></img>
+          <p className="text-small text-muted mt-3">
+            Personalized advice on life
+            <br />
+            and health insurance. No spam
+            <br />
+            and no mis-selling.
+          </p>
+        </div>
+         <button className='p-2 btn btn-primary fs-5 mb-5 mt-5' style={{width:"20%",margin:" 0 auto"}}>Sign up for free</button>
+      </div>
+    </div>
+     );
+}
+
+export default Universe;
